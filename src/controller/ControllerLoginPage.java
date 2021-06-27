f72f1137f0ac42b64640bacf3605f806a2c2815a
Sepehr Beheshti <sepehrbeheshti900@gmail.com>
2021-06-27 19:21:53 +0430

    update project

--- src/controller/ControllerLoginPage.java
+++ src/controller/ControllerLoginPage.java
@@ -1,4 +1,26 @@
 package controller;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXCheckBox;
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import javafx.fxml.FXML;
+
 public class ControllerLoginPage {
+
+    @FXML
+    private JFXPasswordField passwordFLD;
+
+    @FXML
+    private JFXTextField usernameFLD;
+
+    @FXML
+    private JFXButton signupBTN;
+
+    @FXML
+    private JFXCheckBox checkBox;
+
+    @FXML
+    private JFXButton loginBTN;
+
 }
