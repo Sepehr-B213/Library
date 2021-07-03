a2fda449b979edbeacbea60d44c68483f25e2af9
Sepehr Beheshti <sepehrbeheshti900@gmail.com>
2021-07-03 15:47:32 +0430

    Update LoginPage.fxml and LoginPageController

--- src/controller/LoginPageController.java
+++ src/controller/LoginPageController.java
@@ -4,6 +4,7 @@ import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXCheckBox;
 import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
+import javafx.scene.control.Label;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
@@ -28,6 +29,9 @@ public class LoginPageController {
 
     @FXML
     private JFXButton loginBTN;
+
+    @FXML
+    private Label ErrorLBL;
 
 
 }
