b3616e3399b1199465c4d7cb3b939b48f5a0c5d7
Sepehr Beheshti <sepehrbeheshti900@gmail.com>
2021-07-07 17:44:34 +0430

    Adding AdminDashboardPage.fxml and childrenPage of Admin Dashboard FXML and Controllers file

---
+++ src/controller/AdminDashboardPageController.java
@@ -0,0 +1,63 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+
+public class AdminDashboardPageController {
+
+    @FXML
+    private Button homeBTN;
+
+    @FXML
+    private Button usersBTN;
+
+    @FXML
+    private Button reportBTN;
+
+    @FXML
+    private Button menuBTN;
+
+    @FXML
+    private Button inboxBTN;
+
+    @FXML
+    private Button settingsBTN;
+
+    @FXML
+    private Button logoutBTN;
+
+    @FXML
+    private Pane settingPNL;
+
+    @FXML
+    private Pane reportPNL;
+
+    @FXML
+    private Pane userPNL;
+
+    @FXML
+    private Pane homePNL;
+
+    @FXML
+    private TextField searchBTN;
+
+    @FXML
+    private VBox homechldrenPNL;
+
+    @FXML
+    private ImageView minBTN;
+
+    @FXML
+    private ImageView closeBTN;
+
+    @FXML
+    void handleClicks(ActionEvent event) {
+
+    }
+
+}
