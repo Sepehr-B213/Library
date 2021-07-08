87b1f2a212746378ac06faf5bb3f0074579f8b4f
Sepehr Beheshti <sepehrbeheshti900@gmail.com>
2021-07-08 21:37:30 +0430

    Add SettingPage

---
+++ src/controller/SettingPageController.java
@@ -0,0 +1,24 @@
+package controller;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.fxml.FXML;
+
+public class SettingPageController {
+
+    @FXML
+    private JFXTextField passwordChangeFLD;
+
+    @FXML
+    private JFXTextField confirmPasswordFLD;
+
+    @FXML
+    private JFXButton changePasswordBTN;
+
+    @FXML
+    private JFXTextField nameChangeFLD;
+
+    @FXML
+    private JFXButton changeNameBTN;
+
+}
