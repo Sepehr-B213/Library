dd302afeb5244182a68f4a40570a6594f989e708
AminMohammadii <pg.aminmohammadi@gmail.com>
2021-07-12 17:16:14 +0430

    Update LibrarianPageController.

--- src/controller/LoginPageController.java
+++ src/controller/LoginPageController.java
@@ -4,14 +4,11 @@ import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXCheckBox;
 import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
 import model.Relevant;
 import model.Librarian;
 
@@ -21,7 +18,6 @@ import java.util.ResourceBundle;
 
 
 public class LoginPageController extends MainController implements Initializable {
-
     @FXML
     private JFXPasswordField passwordFLD;
 
@@ -32,78 +28,84 @@ public class LoginPageController extends MainController implements Initializable
     private JFXButton signupBTN;
 
     @FXML
-    private JFXCheckBox checkBox;
-
-    @FXML
     private JFXButton loginBTN;
 
     @FXML
     private Label errorLBL;
+
+    @FXML
+    private Button closeBTN;
+
+    @FXML
+    private Button minBTN;
+
+    static Stage stage = null;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
         loginBTN.setOnAction(e -> checkInfo());
 
-        checkBox.setOnAction(event -> loginBTN.setDisable(!checkBox.isSelected()));
-
         signupBTN.setOnAction(event -> {
-            try {
-                show(load("../view/SignupPage.fxml"));
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        });
-
-    }
-
-    public void checkInfo() {
-        if(!usernameFLD.getText().isEmpty() && !passwordFLD.getText().isEmpty()) {
-            if(checkAccountAdmin()) {
-                close(loginBTN);
+            if(stage == null) {
                 try {
-                    show(load("../view/AdminDashboardPage.fxml"));
+                    stage = show(new Stage(), load("../view/SignupPage.fxml"));
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
-            else if (checkAccountLibrarian()) {
+        });
+
+        closeBTN.setOnAction(event -> close(closeBTN));
+
+        minBTN.setOnAction(event -> minimize(minBTN));
+    }
+
+    public void checkInfo() {
+        if(!usernameFLD.getText().isEmpty() && !passwordFLD.getText().isEmpty()) {
+            checkAccountAdmin();
+        }
+        else {
+            clearText(usernameFLD, passwordFLD);
+            errorLBL.setText(Relevant.loginPageErrors[0]);
+        }
+    }
+
+    public void checkAccountAdmin () {
+        if(convertInt(usernameFLD.getText()) == Relevant.admin.getId()) {
+            if (passwordFLD.getText().equals(Relevant.admin.getPassword())) {
+                stage = null;
+                clearText(errorLBL, usernameFLD, passwordFLD);
                 close(loginBTN);
-                // open LibrarianMainPage
+                try {
+                    stage = show(new Stage(), load("../view/AdminDashboardPage.fxml"));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            else {
+                clearText(passwordFLD);
+                errorLBL.setText(Relevant.loginPageErrors[2]);
             }
         }
         else
-            errorLBL.setText(Relevant.loginPageErrors[0]);
+            checkAccountLibrarian();
     }
 
-    public boolean checkAccountAdmin () {
-        if(convertInt(usernameFLD.getText()) == Relevant.admin.getId()) {
-            if(passwordFLD.getText().equals(Relevant.admin.getPassword()))
-                return true ;
-            else
-                errorLBL.setText(Relevant.loginPageErrors[2]);
+    public void checkAccountLibrarian () {
+        Librarian librarian = Librarian.search(convertInt(usernameFLD.getText()));
+        if (librarian == null) {
+            errorLBL.setText(Relevant.loginPageErrors[1]);
         }
-        return false ;
-    }
-
-    public  boolean checkAccountLibrarian () {
-        int id = convertInt(usernameFLD.getText());
-
-        if (id != 0) {
-            Librarian librarian = Librarian.search(id);
-            if (librarian == null) {
-                errorLBL.setText(Relevant.loginPageErrors[1]);
-                return false;
-            }
-            else if(passwordFLD.getText().equals(librarian.getPassword()))
-                return true;
-            else {
-                errorLBL.setText(Relevant.loginPageErrors[2]);
-                return false;
-            }
+        else if(passwordFLD.getText().equals(librarian.getPassword())) {
+            stage = null;
+            clearText(errorLBL, usernameFLD, passwordFLD);
+            close(loginBTN);
+            // open LibrarianMainPage
         }
         else {
-            return false;
+            clearText(passwordFLD);
+            errorLBL.setText(Relevant.loginPageErrors[2]);
         }
     }
 
