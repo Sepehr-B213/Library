54df2ee0135df255bd3253f96380108a679f6dd0
Sepehr Beheshti <sepehrbeheshti900@gmail.com>
2021-07-13 03:40:16 +0430

    Update Report Page

--- src/controller/ReportPageController.java
+++ src/controller/ReportPageController.java
@@ -1,36 +1,47 @@
 package controller;
 
-import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXCheckBox;
 import com.jfoenix.controls.JFXDatePicker;
 import com.jfoenix.controls.JFXTextField;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.layout.VBox;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class ReportPageController extends MainController implements Initializable {
+
     @FXML
     private JFXDatePicker dateFLD;
-
-    @FXML
-    private VBox reportChildrenPNL;
-
-    @FXML
-    private JFXButton dateSearchBTN;
 
     @FXML
     private JFXTextField bookFLD;
 
     @FXML
-    private JFXButton bookSearchBTN;
-
-    @FXML
     private JFXTextField authorFLD;
 
     @FXML
-    private JFXButton searchAuthorBTN;
+    private VBox reportChildrenPNL;
+
+    @FXML
+    private JFXDatePicker dateFLD1;
+
+    @FXML
+    private JFXCheckBox reportCHB;
+
+    @FXML
+    private Button authorSearchBTN;
+
+    @FXML
+    private Button bookSearchBTN;
+
+    @FXML
+    private Button dateSearchBTN;
+
+    @FXML
+    private Button dateSearchBTN1;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
