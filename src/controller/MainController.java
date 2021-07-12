0f7ab8da6d01855b7d925b928c93426a14af56bd
AminMohammadii <pg.aminmohammadi@gmail.com>
2021-07-12 17:18:39 +0430

    Update MainController.

--- src/controller/MainController.java
+++ src/controller/MainController.java
@@ -4,6 +4,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
@@ -13,8 +14,6 @@ import java.io.IOException;
 public class MainController {
 
     private double x,y;
-
-    static Stage stage = null;
 
     public Parent load(String address) throws IOException {
         try {
@@ -27,22 +26,20 @@ public class MainController {
         }
     }
 
-    public void show(Parent parent) {
-        if (stage == null) {
-            stage = new Stage();
-            stage.setScene(new Scene(parent));
-            stage.initStyle(StageStyle.UNDECORATED);
+    public Stage show(Stage stage, Parent parent) {
+        stage.setScene(new Scene(parent));
+        stage.initStyle(StageStyle.UNDECORATED);
 
-            parent.setOnMousePressed(event -> {
-                x = event.getSceneX();
-                y = event.getSceneY();
-            });
-            parent.setOnMouseDragged(event -> {
-                stage.setX(event.getScreenX() - x);
-                stage.setY(event.getScreenY() - y);
-            });
-            stage.show();
-        }
+        parent.setOnMousePressed(event -> {
+            x = event.getSceneX();
+            y = event.getSceneY();
+        });
+        parent.setOnMouseDragged(event -> {
+            stage.setX(event.getScreenX() - x);
+            stage.setY(event.getScreenY() - y);
+        });
+        stage.show();
+        return stage;
     }
 
     public void close(Node node) {
@@ -51,9 +48,22 @@ public class MainController {
 
     public void minimize(Node node) { ((Stage)node.getScene().getWindow()).setIconified(true); }
 
+    public void clearText(Node... nodes) {
+        for(Node node : nodes) {
+            boolean isNotWorked = false;
+            try {
+                ((Label) node).setText("");
+            } catch (Exception e) {
+                isNotWorked = true;
+            }
+            if (isNotWorked) {
+                try {
+                    ((TextField) node).clear();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
 
-    public void clearField(Node... nodes) {
-        for(Node node : nodes)
-            ((TextField)node).clear();
     }
 }
