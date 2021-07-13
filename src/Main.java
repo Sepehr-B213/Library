97773d680920c7b9f2c4165f85ccec450160b71c
amirhosseinymn810 <amirhosseinymn810@gmail.com>
2021-07-13 04:49:44 +0430

    update Main class

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,34 @@
 import com.sun.istack.internal.NotNull;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import model.Relevant;
 
 
 public class Main extends Application {
+    private double x, y;
 
     @Override
     public void start(@NotNull Stage primaryStage) throws Exception{
         FXMLLoader loader = new FXMLLoader(this.getClass().getResource("view/LoginPage.fxml"));
-
         loader.load();
 
         Relevant.loginStage = primaryStage;
         primaryStage.setScene(new Scene(loader.getRoot()));
+        primaryStage.initStyle(StageStyle.UNDECORATED);
+
+        ((Parent)loader.getRoot()).setOnMousePressed(event -> {
+            x = event.getSceneX();
+            y = event.getSceneY();
+        });
+        ((Parent)loader.getRoot()).setOnMouseDragged(event -> {
+            primaryStage.setX(event.getScreenX() - x);
+            primaryStage.setY(event.getScreenY() - y);
+        });
+
         primaryStage.show();
 
     }
